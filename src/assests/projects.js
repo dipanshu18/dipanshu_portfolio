49aa382e8cda5357bc0888ee6148e3f31d4ae747
dipanshu18 <torawanedipanshu@gmail.com>
2023-06-13 22:26:29 +0530

    Added new projects

--- src/assests/projects.js
+++ src/assests/projects.js
@@ -4,7 +4,7 @@ const projects = [
     title: "CollegeBay",
     description:
       "CollegeBay is an online marketplace for college students to buy and sell college-related materials. The platform is designed to make it easy for students to find what they need, sell what they do not, and connect with other students in the same college or university.",
-    codeLink: "",
+    codeLink: "https://github.com/dipanshu18/collegebay",
     demoLink: "",
   },
   {
@@ -12,30 +12,32 @@ const projects = [
     title: "We care for Furs",
     description:
       "This project aims to predict various types of dog skin diseases, a zoonotic disease, by utilizing diverse machine-learning models and detect the type of disease, offer recommendations for precautions that should be taken to prevent the spread of the disease.",
-    codeLink: "",
-    demoLink: "",
+    codeLink: "https://github.com/Vrukshali-26/wecareforfurs",
+    demoLink: "https://github.com/Vrukshali-26/wecareforfurs",
   },
   {
     id: 3,
-    title: "Project Title",
-    description: "Project Description",
-    codeLink: "",
+    title: "Gamei Pedia",
+    description:
+      "This project is aimed towards exploring API for dynamic data of the video games which is provided by RAWG. We have a SPA of upcoming releases, popular releases, and currently released information on the video games with appropriate thumbnails and in-game screenshots for giving the user an overview of the game. This also includes search functionality in which the user can search for a specific video game.",
+    codeLink: "https://github.com/dipanshu18/gamei-pedia",
     demoLink: "",
   },
   {
     id: 4,
-    title: "Project Title",
-    description: "Project Description",
-    codeLink: "",
-    demoLink: "",
+    title: "Capture Portfolio Template",
+    description:
+      "This project is a SPA of portfolio for photographers or videographers in which there are lots of cool visual animations for a smooth user interface. There is a great colour scheme that is applied so that it truly describes their work and it’s mobile responsive.",
+    codeLink: "https://github.com/dipanshu18/capture-portfolio-template",
+    demoLink: "https://capture-portfolio-template.vercel.app/",
   },
   {
     id: 5,
     title: "Portfolio Website",
     description:
       "The current webpage you are surfing is the live demo of the project. Immerse yourself in an engaging visual experience as you navigate through various sections of my portfolio. Discover my different types of projects.",
-    codeLink: "",
-    demoLink: "",
+    codeLink: "https://github.com/dipanshu18/dipanshu_portfolio",
+    demoLink: "https://dipanshu-portfolio-dev.vercel.app/",
   },
 ];
 
