53bb18f383cd7876b376c33e9040d501ba3039ac
dipanshu18 <torawanedipanshu@gmail.com>
2023-06-13 22:32:42 +0530

    edited project cards

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -10,7 +10,7 @@ export default function ProjectCard({
   return (
     <m.div
       whileHover={{ scale: 1.05 }}
-      className="bg-red-300 text-red-900 rounded-lg p-10 shadow-2xl shadow-red-300 max-w-[400px]"
+      className="bg-red-300 text-red-900 rounded-lg p-10 shadow-2xl shadow-red-300 max-w-[400px] h-[350px]"
     >
       <h1 className="font-bold text-xl lg:text-3xl">{title}</h1>
       <p className="font-medium text-red-500 py-4 text-sm">{description}</p>
